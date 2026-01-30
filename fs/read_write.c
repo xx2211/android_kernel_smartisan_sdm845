378d2084dbe4968e692647438d6d5d5d99ab915a
xx2211 <65813268+xx2211@users.noreply.github.com>
2026-01-30 09:42:54 +0800

    Add call to ksu_handle_vfs_read in vfs_read function

--- fs/read_write.c
+++ fs/read_write.c
@@ -460,6 +460,8 @@ ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 {
 	ssize_t ret;
 
+	ksu_handle_vfs_read(file, buf, count, pos);
+
 	if (!(file->f_mode & FMODE_READ))
 		return -EBADF;
 	if (!(file->f_mode & FMODE_CAN_READ))
