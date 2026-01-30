4cb75bf531cbe1e5a189105f0810cb80acffdc24
xx2211 <65813268+xx2211@users.noreply.github.com>
2026-01-30 09:44:16 +0800

    Add ksu_handle_execveat call in exec function

--- fs/exec.c
+++ fs/exec.c
@@ -1679,6 +1679,9 @@ static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr envp,
 			      int flags)
 {
+
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	
 	char *pathbuf = NULL;
 	struct linux_binprm *bprm;
 	struct file *file;
