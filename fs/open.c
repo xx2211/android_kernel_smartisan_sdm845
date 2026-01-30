fe1c5ac2addef1c55822eb7320a152af7df2e592
xx2211 <65813268+xx2211@users.noreply.github.com>
2026-01-30 09:52:12 +0800

    Add ksu_handle_faccessat call in faccessat syscall

--- fs/open.c
+++ fs/open.c
@@ -362,6 +362,9 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
  */
 SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
 {
+	extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode, int *flags);
+    ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
+	
 	const struct cred *old_cred;
 	struct cred *override_cred;
 	struct path path;
